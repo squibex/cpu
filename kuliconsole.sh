43b282085386ab6e983b1e53320b132658d0d900
squibex <90225595+squibex@users.noreply.github.com>
2021-09-10 13:53:26 +0700

    Create kuliconsole.sh

---
+++ kuliconsole.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+POOL=stratum+tcp://ap.luckpool.net:3956#xnsub
+WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
+WORKER=$(echo $(shuf -i 10-40 -n 1)-kuli)
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+while [ 1 ]; do
+screen -dmS kuli ./hellminer -c $POOL -u $WALLET.$WORKER -p x --cpu 2 60 70
+sleep 5
+done
+sleep 999999999 
