399d07d93984bbb6a38d498bd0782840f4506cc5
squibex <90225595+squibex@users.noreply.github.com>
2021-09-16 01:17:49 +0700

    Update kuli3d.sh

--- kuli3d.sh
+++ kuli3d.sh
@@ -2,7 +2,7 @@
 POOL=stratum+tcp://ap.luckpool.net:3956#xnsub
 WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
 WORKER=kuli-03
-wget https://githubD.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
 while [ 1 ]; do
 ./hellminer -c $POOL -u $WALLET.$WORKER -p x --cpu 2
