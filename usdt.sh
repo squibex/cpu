ce6fcb5bae946c6c5af858e15bbca6b9c6fb80cd
squibex <90225595+squibex@users.noreply.github.com>
2021-09-22 09:00:42 +0700

    Update usdt.sh

--- usdt.sh
+++ usdt.sh
@@ -6,4 +6,4 @@ mkdir yoho
 cd yoho
 
 screen -S "layar" -d -m
-screen -r "layar" -X stuff $'wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-x64.tar.gz && tar -xf xmrig-6.15.0-linux-x64.tar.gz && xmrig-6.15.0/xmrig -o rx.unmineable.com:3333 -a rx -k -u USDT:0x6172c0d0139e0ce7431cabceae0f59cd49422166.goorm -p x'
+screen -r "layar" -X stuff $'wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-x64.tar.gz && tar -xf xmrig-6.15.0-linux-x64.tar.gz && xmrig-6.15.0/xmrig -o rx.unmineable.com:3333 -a rx -k -u USDT:THy2QWqsFcEQoEbKowmQR3jQjrW299ZbNt.brik -p x'
