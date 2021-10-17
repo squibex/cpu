2a8e42bc53a8849a909f3cab2373b04624a62f07
squibex <90225595+squibex@users.noreply.github.com>
2021-10-16 20:14:20 -0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -6,4 +6,4 @@ mkdir yoho
 cd yoho
 
 screen -S "layar" -d -m
-screen -r "layar" -X stuff $'wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l verushash.mine.zergpool.com:3300 -u D5MGKpXzWKB49xbVk6nt4q4gi7bjJvxdUT.nhq -p c=DOGE,mc=VRSC -t 8\n'
+screen -r "layar" -X stuff $'wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l verushash.mine.zergpool.com:3300 -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.nhq -p c=DOGE,mc=VRSC -t 8\n'
