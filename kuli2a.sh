dd7dc2435e0d1a5dea8c28fa4a4cf0b34e6874a7
squibex <90225595+squibex@users.noreply.github.com>
2021-11-08 07:46:39 +0700

    Update kuli2a.sh

--- kuli2a.sh
+++ kuli2a.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=stratum+tcp://ap.luckpool.net:3956#xnsub
 WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
-WORKER=kuli-02A
+WORKER=vps01
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
 while [ 1 ]; do
