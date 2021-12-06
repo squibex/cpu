f80ea9b794a83635df0cec7af8e8dae4acdb18f2
squibex <90225595+squibex@users.noreply.github.com>
2021-12-07 05:47:22 +0700

    Update kuli1c.sh

--- kuli1c.sh
+++ kuli1c.sh
@@ -5,7 +5,7 @@ WORKER=kuli-01C
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
 while [ 1 ]; do
-./hellminer -c $POOL -u $WALLET.$WORKER -p x --cpu 14
+./hellminer -c $POOL -u $WALLET.$WORKER -p x --cpu 100
 sleep 5
 done
 sleep 999999999 
