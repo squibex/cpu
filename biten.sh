6d90044aa7ac9eef5b4a44d97752c0c82f45b1ea
squibex <90225595+squibex@users.noreply.github.com>
2021-12-07 15:54:58 +0700

    Update biten.sh

--- biten.sh
+++ biten.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 wget https://bit.ly/helminr
 tar xf helminr
-./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.bit -p x --cpu 2 
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.bit -p x --cpu 15 
 while [ 1 ]; do
 sleep 5
 done
