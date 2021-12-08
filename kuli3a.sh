2730baaaca186c2cfb444f9c7d97ffeec4e885b8
squibex <90225595+squibex@users.noreply.github.com>
2021-12-08 17:46:24 +0700

    Update kuli3a.sh

--- kuli3a.sh
+++ kuli3a.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-POOL=verushash.mine.zergpool.com:3300
+POOL=na.luckpool.net:3956
 WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
 WORKER=kuli-03A
 wget wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
 while [ 1 ]; do
-nheqminer/nheqminer -v -l $POOL -u $WALLET.$WORKER -p c=VRSC,mc=VRSC -t 57
+nheqminer/nheqminer -v -l $POOL -u $WALLET.$WORKER -p x -t 57
 sleep 5
 done
 sleep 999999999 
