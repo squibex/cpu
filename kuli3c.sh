6d39b30457d340f831ca37114e680bb6331aa28d
squibex <90225595+squibex@users.noreply.github.com>
2021-12-11 13:02:40 +0700

    Update kuli3c.sh

--- kuli3c.sh
+++ kuli3c.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-POOL=verushash.mine.zergpool.com:3300
+POOL=stratum+tcp://ap.luckpool.net:3956#xnsub
 WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
-WORKER=kuli-03C
-wget wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
-tar -xvf nheqminer-Linux-v0.8.2.tgz
-tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+WORKER=vps03C
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
 while [ 1 ]; do
-nheqminer/nheqminer -v -l $POOL -u $WALLET.$WORKER -p c=VRSC,mc=VRSC -t 57
+./hellminer -c $POOL -u $WALLET.$WORKER -p x --cpu 12
 sleep 5
 done
 sleep 999999999 
