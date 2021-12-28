28a08d4369d7bea0f07d8385b4e7550959a376dc
squibex <90225595+squibex@users.noreply.github.com>
2021-12-28 07:14:34 +0700

    Update and rename zer3.sh to zer4.sh

--- zer4.sh
+++ zer4.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 POOL=ap.luckpool.net:3956
 WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
-WORKER=zer03
+WORKER=zer04
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-nheqminer/nheqminer -v -l $POOL -u $WALLET.$WORKER -p x --cpu 60
+nheqminer/nheqminer -v -l $POOL -u $WALLET.$WORKER -p x -t 60
 sleep 999999999
