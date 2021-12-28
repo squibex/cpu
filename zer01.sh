9f3395243ed003ebcb3c2a9a86314da480afde53
squibex <90225595+squibex@users.noreply.github.com>
2021-12-28 07:03:49 +0700

    Update zer01.sh

--- zer01.sh
+++ zer01.sh
@@ -2,7 +2,7 @@
 POOL=stratum+tcp://ap.luckpool.net:3956#xnsub
 WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
 WORKER=zer01
-https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
 nheqminer/nheqminer -v -l $POOL -u $WALLET.$WORKER -p x --cpu 60
