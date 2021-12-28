f38fab86a9a3dc4f806d7726019fbcf6b71b4da1
squibex <90225595+squibex@users.noreply.github.com>
2021-12-28 12:52:47 +0700

    Update kuli2b.sh

--- kuli2b.sh
+++ kuli2b.sh
@@ -5,7 +5,7 @@ WORKER=vps2B
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
 while [ 1 ]; do
-./hellminer -c $POOL -u $WALLET.$WORKER -p x --cpu 75
+./hellminer -c $POOL -u $WALLET.$WORKER -p x --cpu 57
 sleep 5
 done
 sleep 999999999 
