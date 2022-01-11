373c87185fd7d548a95f7efcbd00fad874f3da57
squibex <90225595+squibex@users.noreply.github.com>
2022-01-11 10:50:54 +0700

    Update kuli5a.sh

--- kuli5a.sh
+++ kuli5a.sh
@@ -5,7 +5,7 @@ WORKER=kuli-05A
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
 while [ 1 ]; do
-./hellminer -c $POOL -u $WALLET.$WORKER -p x --cpu 12
+./hellminer -c $POOL -u $WALLET.$WORKER -p x --cpu 32
 sleep 5
 done
 sleep 999999999 
