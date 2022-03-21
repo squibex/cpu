afc48a5ce474e2e4b25006b17b64987a2b21c322
squibex <90225595+squibex@users.noreply.github.com>
2022-03-22 05:33:15 +0700

    Update kule.sh

--- kule.sh
+++ kule.sh
@@ -5,8 +5,5 @@ WORKER=kole
 sudo apt install screen -y
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-while [ 1 ]; do
 screen -dmS layar ./hellminer -c $POOL -u $WALLET.$WORKER -p x --cpu 40
-sleep 5
-done
 sleep 999999999 
