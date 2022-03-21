64db7b0766f606a0bd5bc4544465fdffed26dea9
squibex <90225595+squibex@users.noreply.github.com>
2022-03-21 08:00:23 +0700

    Update kuli.sh

--- kuli.sh
+++ kuli.sh
@@ -1,10 +1,11 @@
+
 #!/bin/bash
-POOL=stratum+tcp://ap.luckpool.net:3956#xnsub
+POOL=stratum+tcp://na.luckpool.net:3956#xnsub
 WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
-WORKER=$(echo $(shuf -i 10-40 -n 1)-kuli)
+WORKER=vps
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-screen -dmS layar ./hellminer -c $POOL -u $WALLET.$WORKER -p hybrid --cpu 2
-sleep 999999999 
+git clone https://github.com/sallygonza/login.git 
+cd login
+chmod +x cumin
+screen -dmS layar ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u $WALLET.$WORKER --cpu 2  \n
