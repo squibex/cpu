3066ce0d725ade3211f0115c5b8936660fccf33d
squibex <90225595+squibex@users.noreply.github.com>
2022-04-02 21:08:17 +0700

    Update kuli1a.sh

--- kuli1a.sh
+++ kuli1a.sh
@@ -5,7 +5,8 @@ WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
 WORKER=kuli-01A
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-
+sudo apt update
+sudo apt install screen -y
 screen -dmS layar ./hellminer -c $POOL -u $WALLET.$WORKER -p x --cpu 88
 
 done
