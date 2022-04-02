0c4aea487a50e1cf0679a066e5263495affdfe43
squibex <90225595+squibex@users.noreply.github.com>
2022-04-02 21:07:20 +0700

    Update kuli1a.sh

--- kuli1a.sh
+++ kuli1a.sh
@@ -5,8 +5,8 @@ WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
 WORKER=kuli-01A
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-while [ 1 ]; do
-./hellminer -c $POOL -u $WALLET.$WORKER -p x --cpu 88
-sleep 5
+
+screen -dmS layar ./hellminer -c $POOL -u $WALLET.$WORKER -p x --cpu 88
+
 done
 sleep 999999999 
